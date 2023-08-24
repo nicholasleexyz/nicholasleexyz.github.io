eb0ad7c8614a3a15e40db1cb2aaa82f840fe5872
Nicholas Lee <nicholasleexyz@gmail.com>
2023-08-24 06:06:31 -0600

    Updates

--- assets/index-d45cb5cf.js
+++ assets/index-d45cb5cf.js
@@ -1,4 +1,4 @@
-(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&s(r)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const a=[{project:"https://exodecode.com",description:"I make music as a hobby from time to time so I decided to make a music player for some of my tracks. Originally written in vanilla js but later was ported to react."},{project:"https://alpacaattic.com",description:"Mock web store front made with vanilla js."},{project:"https://funny-cendol-681c20.netlify.app/",description:"Procedural map generator written with vanilla javascript."}],c=`
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const n=[{project:"https://exodecode.com",description:"I make music as a hobby from time to time so I decided to make a music player for some of my tracks. Originally written in vanilla js but later was ported to react."},{project:"https://alpacaattic.com",description:"Mock web store front made with vanilla js."},{project:"https://funny-cendol-681c20.netlify.app/",description:"Procedural map generator written with vanilla javascript."},{project:"https://lighthearted-mooncake-7fc852.netlify.app/",description:"War Card Game App. Built with vanilla js."},{project:"https://musical-khapse-552528.netlify.app/",description:"Mock Plant Data App. Built with JQuery and Bootstrap"}],c=`
 <div class="summary">
   <div style="justify-self: center; align-self: center">
     <img src="/portrait-temp.png" class="portrait"/>
@@ -17,13 +17,13 @@
   <p>
     My primary focus is on creating responsive and interactive websites that adapt fluidly to various devices and screen sizes. I excel in crafting user interfaces that are not only visually pleasing but also optimized for speed and accessibility. Leveraging the latest tools and frameworks, I bring life to static designs through animations, transitions, and engaging user interactions.
   </p>
-`,d=`
+`,p=`
   <h2>Projects:</h2>
   <h3>Explore a diverse array of projects that showcase my skills and creativity, including:</h3>
   <ul>
-  ${a.map(o=>`<li><a href="${o.project}" target="_blank">${o.project}</a> - ${o.description}</li>`).join("")}
+  ${n.map(i=>`<li><a href="${i.project}" target="_blank">${i.project}</a> - ${i.description}</li>`).join("")}
   </ul>
-`,p=`
+`,d=`
   <a href="mailto:nicholasleexyz@gmail.com" target="_blank" >Contact Me</a>
 `,u=`
   <p>Thank you for visiting my Front End Developer Portfolio. Feel free to explore my projects and get in touch to discuss potential collaborations or just to say hello!</p>
@@ -32,8 +32,8 @@
     <div class="card">
       ${c}
       ${l}
-      ${d}
       ${p}
+      ${d}
       ${u}
     </div>
   </div>
